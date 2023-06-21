2d8334796f1a3e3d3fd44844fe99dedcba8cd471
seth <getchoo@tuta.io>
2023-06-20 23:04:21 -0400

    feat: add nix flake

---
+++ flake.nix
@@ -0,0 +1,86 @@
+{
+  description = "ni, implemented in bash";
+
+  inputs = {
+    nixpkgs.url = "nixpkgs/nixos-unstable";
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+    ...
+  }: let
+    version = builtins.substring 0 8 self.lastModifiedDate;
+
+    systems = [
+      "x86_64-linux"
+      "aarch64-linux"
+      "x86_64-darwin"
+      "aarch64-darwin"
+    ];
+
+    forAllSystems = nixpkgs.lib.genAttrs systems;
+    nixpkgsFor = forAllSystems (system:
+      import nixpkgs {
+        inherit system;
+        overlays = [self.overlays.default];
+      });
+
+    forEachSystem = fn:
+      forAllSystems (system:
+        fn {
+          inherit system;
+          pkgs = nixpkgsFor.${system};
+        });
+  in {
+    devShells = forEachSystem ({pkgs, ...}: let
+      inherit (pkgs) mkShell;
+    in {
+      default = mkShell {
+        packages = with pkgs; [
+          beautysh
+          gum
+          shellcheck
+        ];
+      };
+    });
+
+    formatter = forEachSystem ({pkgs, ...}: pkgs.alejandra);
+
+    packages = forEachSystem ({pkgs, ...}: {
+      inherit (pkgs) nish;
+      default = pkgs.nish;
+    });
+
+    overlays.default = _: prev: let
+      inherit (prev) stdenv;
+      inherit (prev.lib) licenses maintainers platforms;
+    in {
+      nish = stdenv.mkDerivation rec {
+        pname = "nish";
+        inherit version;
+
+        src = builtins.path {
+          name = "${pname}-src";
+          path = ./.;
+        };
+
+        buildInputs = with prev; [gum];
+
+        installPhase = ''
+          mkdir -p $out/bin/lib
+          install -Dm755 n{i,r} $out/bin
+          install -Dm755 lib/package_manager.sh $out/bin/lib
+        '';
+
+        meta = {
+          description = "ni, implemented in bash";
+          homepage = "https://github.com/ryanccn/nish";
+          license = licenses.mit;
+          maintainers = [maintainers.getchoo];
+          platforms = with platforms; linux ++ darwin;
+        };
+      };
+    };
+  };
+}
